647195f68026856a75a6265f443f901cb6c8a8ce
ArthurLps77 <arthurlsampaios@gmail.com>
2025-09-24 16:19:46 -0300

    Add files via upload

---
+++ Modulo 1/Extra/01try_except.py
@@ -0,0 +1,7 @@
+
+# de uma maneira mais amigável ao usuário
+
+try: # o código tenta executar o comando
+    resultado = 10/0
+except: # caso não consiga, ele apresenta qual é o erro.
+    print('Ocorreu um erro na operação. Não é possível a divisão com denominador igual a zero.')
